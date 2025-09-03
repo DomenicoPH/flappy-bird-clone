8d6cc55d453192c10f0b209fa2957a01bc6a94ba
Domenico Pagano <dpaganoh@gmail.com>
2025-09-02 23:40:42 -0500

    multiple pipes

--- src/index.js
+++ src/index.js
@@ -20,14 +20,16 @@ const config = {
 
 // VARIABLES
 const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
 
 let bird = null;
 
 let upperPipe = null;
 let lowerPipe = null;
+
 const pipeVerticalDistanceRange = [150, 250];
-let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-let pipeVerticalPosition = Phaser.Math.Between(50, config.height - 50 - pipeVerticalDistance);
+const pipeHorizontalDistanceRange = [380, 420];
+let pipeHorizontalDistance = 0;
 
 const flapVelocity = 300;
 const initialBirdPosition = { x: config.width / 10, y: config.height / 2 };
@@ -43,8 +45,19 @@ function create(){
   this.add.image(0, 0, 'sky').setOrigin(0, 0); //Background
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0); //Bird
   bird.body.gravity.y = 400;
-  upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, 'pipe').setOrigin(0, 1); //Upper Pipe
-  lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0); //Lower Pipe
+
+  for(let i=0; i<PIPES_TO_RENDER; i++){
+    // Create pipes
+    pipeHorizontalDistance += Phaser.Math.Between(...pipeHorizontalDistanceRange);
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(50, config.height - 50 - pipeVerticalDistance);
+    
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(pipeHorizontalDistance, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+    
+    upperPipe.body.velocity.x = -VELOCITY;
+    lowerPipe.body.velocity.x = -VELOCITY;
+  }
 
   // CONTROLES
   this.input.on('pointerdown', flap); //Click
