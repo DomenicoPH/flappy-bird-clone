37bf6ff7981ff9bad04503a76450323c993d11c8
Domenico Pagano <dpaganoh@gmail.com>
2025-09-02 22:23:00 -0500

    restartBirdPosition

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      //gravity: { y: 200 },
+      gravity: { y: 400 },
       debug: true,
     }
   },
@@ -23,22 +23,45 @@ function preload(){
   this.load.image('bird', 'assets/bird.png');
 };
 
-let bird = null;
-let totalDelta = null;
-
 const VELOCITY = 200;
 
+let bird = null;
+const flapVelocity = 300;
+const initialBirdPosition = { x: config.width / 10, y: config.height / 2 };
+
 function create(){
-  this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  bird = this.physics.add.sprite(config.width / 10 , config.height / 2, 'bird').setOrigin(0, 0);
-  bird.body.velocity.x = VELOCITY;
+  this.add.image(0, 0, 'sky').setOrigin(0, 0); //Background
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0, 0); //Bird
+
+  // CONTROLES
+  this.input.on('pointerdown', flap); //Click
+  this.input.keyboard.on('keydown-SPACE', flap); //Spacebar
+
 };
 
 function update(time, delta){
-  pingpong();
+  if(bird.y > config.height || bird.y < -bird.height){
+    restartBirdPosition();
+  }
 };
 
-// Otras funciones...
+// FUNCIONES
+
+function restartBirdPosition(){
+  bird.setPosition(initialBirdPosition.x, initialBirdPosition.y);
+  bird.body.velocity.set(0);
+};
+
+function flap(){
+  bird.body.velocity.y = -VELOCITY;
+};
+
+
+
+
+
+// ***** ---------------------------- Otras funciones...
+
 function pingpong(){
   if(bird.x + bird.width > config.width){ bird.body.velocity.x = -VELOCITY } 
   else if(bird.x < 0){ bird.body.velocity.x = VELOCITY }
