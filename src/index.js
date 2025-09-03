2e5d506eee45c25865406ae398cbbe1df62c0cae
Domenico Pagano <dpaganoh@gmail.com>
2025-09-03 08:45:50 -0500

    shared config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import Phaser from "phaser";
 import PlayScene from "./scenes/PlayScene";
 
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = { x: WIDTH * 0.1, y: HEIGHT / 2 };
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION
+};
+
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
   physics: {
     default: 'arcade',
     arcade: {
@@ -13,7 +21,7 @@ const config = {
     }
   },
   scene: [
-    PlayScene
+    new PlayScene(SHARED_CONFIG)
   ]
 };
 
